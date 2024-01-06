ddec7ca0162b5f82009c7f7410052557ade597b4
shehab267 <shehabelhawary267@gmail.com>
2024-01-07 00:33:22 +0200

    Adding Readme file

---
+++ README.md
@@ -0,0 +1,16 @@
+# Simple Online Chat with Socket.io
+
+## About
+
+This repository is a simple online chat project created as part of the tutorial "Learn Socket.io In 30 Minutes" by Web Dev Simplified. Dive into the world of real-time communication using Socket.io and build your own chat applications with ease.
+
+## Tutorial Link
+
+[Learn Socket.io In 30 Minutes](https://www.youtube.com/watch?v=ZKEqqIO7n-k)
+
+## Credits
+
+- Tutorial by **Web Dev Simplified**
+- Repository created by **Shehab Elhawari**
+
+Feel free to explore, experiment, and enhance this project as you learn more about Socket.io!
--- client/script.js
+++ client/script.js
@@ -1,35 +1,35 @@
-import { io } from "socket.io-client";
-const joinRoomButton = document.getElementById("room-button");
-const messageInput = document.getElementById("message-input");
-const roomInput = document.getElementById("room-input");
-const form = document.getElementById("form");
+import { io } from 'socket.io-client';
+const joinRoomButton = document.getElementById('room-button');
+const messageInput = document.getElementById('message-input');
+const roomInput = document.getElementById('room-input');
+const form = document.getElementById('form');
 
-const socket = io("http://localhost:3000");
+const socket = io('http://localhost:3000');
 
-socket.on('connect',() => {
-    displayMessage(`You Connected with Socket ID: ${socket.id}`)
-}); 
-socket.on('receive-message', message => {
-    displayMessage(message);
-})
-form.addEventListener("submit", e => {
-    e.preventDefault();
-    const message = messageInput.value;
-    const room = roomInput.value;
-    if (message === "") return;
-    displayMessage(message);
-    socket.emit("send-message",message, room);
+socket.on('connect', () => {
+  displayMessage(`You Connected with Socket ID: ${socket.id}`);
+});
+socket.on('receive-message', (message) => {
+  displayMessage(message);
+});
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const message = messageInput.value;
+  const room = roomInput.value;
+  if (message === '') return;
+  displayMessage(message);
+  socket.emit('send-message', message, room);
 
-    messageInput.value = "";
+  messageInput.value = '';
 });
 
-joinRoomButton.addEventListener("click", () => {
-    const room = roomInput.value;
-    socket.emit("join-room", room);
+joinRoomButton.addEventListener('click', () => {
+  const room = roomInput.value;
+  socket.emit('join-room', room);
 });
 
 function displayMessage(message) {
-    const div = document.createElement("div");
-    div.textContent = message;
-    document.getElementById("message-container").append(div);
+  const div = document.createElement('div');
+  div.textContent = message;
+  document.getElementById('message-container').append(div);
 }
